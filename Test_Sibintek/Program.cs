0dd5696da298e092194ac7ec0403a3b756bcbf06
Rustem Bikbaev <ibikbaev97@gmail.com>
2019-03-11 23:20:07 +0500

    Начинаем распараллеливание

--- Test_Sibintek/Program.cs
+++ Test_Sibintek/Program.cs
@@ -5,71 +5,68 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.IO;
+using System.Threading;
 
 namespace Test_Sibintek
 {
-    class Program
+    public class Program
     {
-
+        //object block = new object();
         static void Main(string[] args)
         {
-            
-
-            //string path1 = @"C:\Users\Рустем\Desktop\Test_Sibintek\file_for_test_sibintek.txt";
-            //string path2 = @"C:\Users\Рустем\Desktop\Test_Sibintek\file_for_test_sibintek2.png";
-            //string path3 = @"C:\Users\Рустем\Desktop\Test_Sibintek";
-            string path = @"C:\Users\Рустем\Desktop\ASUS";
-            
-
-            //using (StreamReader sr = new StreamReader(path1))
-            //    {
-                    
-            //        Console.WriteLine($"\n Hello, {MD5Hash(sr.ReadToEnd())}");
-            //    }
-
-            //using (StreamReader sr = new StreamReader(path2))
-            //{
-                
-            //    Console.WriteLine($"\n Hello, {MD5Hash(sr.ReadToEnd())}");
-            //}
-            
+            string path = @"I:\Sibintek_test-master";
 
             if (Directory.Exists(path))
             {
-               
+                //// Создаем 4 потоков
+                //Thread[] threads = new Thread[4];
+                //threads[0] = new Thread(new ThreadStart());
 
+                //for (int i = 0; i < 4; i++)
+                //{
+                //    threads[i] = new Thread(new ThreadStart(/*mt.ThreadNumbers*/));
+                //    threads[i].Name = string.Format("Работает поток: #{0}", i);
+                //}    
+                //// Запускаем все потоки
+                //foreach (Thread t in threads)
+                //    t.Start();
+
+                List<string> ErrorList = new List<string>();
                 List<string> filesList = GetFilesList(path);
+                List<string> HashList = new List<string>();
+
                 for (int i = 0; i < filesList.Count; i++)
                 {
-                    using (StreamReader sr = new StreamReader(filesList[i]))
+                    try
                     {
-                        try
-                        {
-                            Console.WriteLine(filesList[i]);
-                            Console.WriteLine( MD5Hash(sr.ReadToEnd()) + "\n");
-                        }
-                        catch (IOException)
-                        {
-                            sr.Close();
-                            continue;
-                        }
-                    }
+                        byte[] buff = File.ReadAllBytes(filesList[i]); //побайтовое чтение файла
 
+                        //вызов 2-3 потока
+                        HashList.Add(MD5Hash(buff));//хеширование
+                        Console.WriteLine(MD5Hash(buff)); //хеширование
+
+                        Console.WriteLine(filesList[i]); //путь  
+
+                        ErrorList.Add("No Exception"); //ошибка
+                        Console.WriteLine(ErrorList[i]); //ошибка
+                    }
+                    catch (Exception e)
+                    {
+                        ErrorList.Add(e.Message);
+                        Console.WriteLine(ErrorList[i]);
+                        continue;
+                    }
                 }
             }
             else
             {
                 Console.WriteLine("Такого пути нет");
             }
-            
-
-
         }
-
 
         private static List<string> GetFilesList(string path) //список путей ко всем файлам
         {
-          
+
             List<string> filesList = new List<string>();
             string[] dirs = Directory.GetDirectories(path);
             filesList.AddRange(Directory.GetFiles(path));
@@ -81,17 +78,15 @@ namespace Test_Sibintek
                 }
                 catch { }
             }
-            
+
             return filesList;
         }
 
-       
-
-        public static string MD5Hash(string input) //функция хеширования файла
+        public static string MD5Hash(byte[] bytes2) //функция хеширования файла
         {
             StringBuilder hash = new StringBuilder();
             MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
-            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+            byte[] bytes = md5provider.ComputeHash(bytes2);
 
             for (int i = 0; i < bytes.Length; i++)
             {
@@ -99,7 +94,5 @@ namespace Test_Sibintek
             }
             return hash.ToString();
         }
-
-      
     }
 }
